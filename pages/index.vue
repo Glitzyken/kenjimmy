abe26baf7153d5f19727afcf95adce3ed95a6e7e
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-08-15 14:51:17 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="test"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+
+.test {
+  width: 5rem;
+  height: 5rem;
+  background-color: orangered;
+}
+</style>
