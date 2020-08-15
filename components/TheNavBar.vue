80f6e8b3c1889d585f471e6475c19187b07e9bce
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-08-15 21:03:13 +0100

    make everything responsive at this point

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-full flex">
-    <div class="text-gray-700 text-sm flex-1">
-      <span class="font-bold mr-4">
+  <div class="w-full md:flex">
+    <div class="text-gray-700 text-sm md:flex-1">
+      <span class="font-bold md:mr-4">
         Call me (+234) 8139113069 📳
       </span>
       /
-      <span class="font-bold ml-4">
+      <span class="font-bold md:ml-4">
         kenjimmy@gmail.com 📭
       </span>
     </div>
--- components/TheSideBar.vue
+++ components/TheSideBar.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <div class="w-32">
-      <img class="w-12" src="../assets/images/kjlogo.png" alt="logo" />
+      <img class="w-12 pb-2" src="../assets/images/kjlogo.png" alt="logo" />
+      <div class="md:mt-64">
+        <div class="md:bg-pink-300 md:h-6 md:w-6"></div>
+        <div class="md:bg-green-300 md:h-6 md:w-6"></div>
+        <div class="md:bg-yellow-300 md:h-6 md:w-6"></div>
+      </div>
     </div>
   </div>
 </template>
