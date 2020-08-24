d86bdbd5aac22c459c92ac8e3e016b802c2f1f8d
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-08-24 12:01:34 +0100

    change button text to download my cv

--- components/Hero.vue
+++ components/Hero.vue
@@ -10,7 +10,7 @@
       <button
         class="bg-kjColorPrime py-2 px-4 uppercase mt-10 rounded-lg text-kjColorLight font-medium text-xs transition duration-200 hover:bg-kjColorDark focus:outline-none"
       >
-        Let's chat!
+        Download my CV
       </button>
     </div>
     <div class="mt-10 md:mt-0 md:flex-1">
