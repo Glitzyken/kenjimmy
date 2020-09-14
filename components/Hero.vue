3a724a1f263409713a94691a4445794ee8da65a8
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-09-14 22:08:26 +0100

    change callout delay tfrom 1 to 0.5

--- components/Hero.vue
+++ components/Hero.vue
@@ -64,7 +64,7 @@ export default {
           duration: 1,
           ease: 'elastic.out(1, 0.3)'
         },
-        1
+        0.5
       )
         .to('.letter', { fontSize: '3rem', duration: 0.05, stagger: 0.1 })
         .to('.des', { opacity: 1, y: 0, duration: 0.7 })
