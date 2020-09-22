03a04d1291bb11ae5bf2895d7fd4ce606009006a
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-09-22 12:51:36 +0100

    add Portfolio component placeholders

---
+++ components/PortfolioCards.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div class="h-128 bg-gray-500">
+        some
+      </div>
+      <div class="h-128 bg-gray-500">
+        another
+      </div>
+      <div class="h-128 bg-gray-500">
+        another
+      </div>
+      <div class="h-128 bg-gray-500">
+        another
+      </div>
+      <div class="h-128 bg-gray-500">
+        another
+      </div>
+    </div>
+  </div>
+</template>
--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    My Works
+    <h1 class="mt-6 text-2xl font-bold capitalize">My works</h1>
+    <CustomBorder />
+    <div class="mt-8">
+      <PortfolioCards />
+    </div>
   </div>
 </template>
