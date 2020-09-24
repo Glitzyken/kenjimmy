475f33baf9637aded5d6f3384f97dc928fd770c3
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-09-24 15:36:03 +0100

    designing portfolio page

--- components/PortfolioCards.vue
+++ components/PortfolioCards.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-      <div class="h-128 bg-gray-500">
-        some
-      </div>
-      <div class="h-128 bg-gray-500">
-        another
-      </div>
-      <div class="h-128 bg-gray-500">
-        another
-      </div>
-      <div class="h-128 bg-gray-500">
-        another
-      </div>
-      <div class="h-128 bg-gray-500">
-        another
+      <div class="h-128 rounded-lg card graphics">
+        <div class="p-8 md:flex justify-between align-bottom">
+          <h2 class="font-bold text-lg capitalize">
+            graphic design
+          </h2>
+          <p class="text-xs capitalize mt-2">
+            photoshop illustrator indesign
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.graphics {
+  background: url('../assets/images/My-works/graphics.jpg');
+  background-size: cover;
+  background-position: center;
+}
+</style>
