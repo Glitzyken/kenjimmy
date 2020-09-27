f41a0f97adce9390a6a5771d2df211c936452c3e
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-09-27 21:03:01 +0100

    add link to stay connected elements

--- components/StayConnected.vue
+++ components/StayConnected.vue
@@ -1,16 +1,20 @@
 <template>
   <section class="max-w-xs m-auto mt-32 text-center">
-    <div class="capitalize font-bold">
+    <div class="capitalize font-bold text-lg">
       stay connected
     </div>
-    <div class="text-gray-600 text-sm mt-6">
+    <div class="text-gray-600 text-sm mt-8">
       Lagos, Nigeria
     </div>
-    <div class="mt-2 font-medium text-xl">
-      kenjimmy17@gmail.com
+    <div class="mt-4 font-medium text-xl">
+      <a href="mailto:kenjimmy17@gmail.com">
+        kenjimmy17@gmail.com
+      </a>
     </div>
-    <div class="text-sm mt-2">
-      (+234) 813 - 9113 - 069
+    <div class="text-sm mt-4">
+      <a href="tel:+2348139113069">
+        (+234) 813 - 9113 - 069
+      </a>
     </div>
     <div class="mt-12 flex justify-center">
       <div class="w-8 h-8 rounded-full">
