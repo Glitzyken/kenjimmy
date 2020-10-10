5645bb6faff7b1f20c0982f69bd9622fa3f19650
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-10-10 16:46:15 +0100

    start deploy repo and change commands in package.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'server',
+  target: 'static',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
