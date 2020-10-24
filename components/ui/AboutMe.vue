9c6b31eb9a851e3f2e2402c2209daf832652780b
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-10-24 09:36:52 +0100

    add about me section

--- components/ui/AboutMe.vue
+++ components/ui/AboutMe.vue
@@ -3,34 +3,40 @@
     <div class="select-none mb-6 w-full">
       <div class="relative pb-64 bg-img">
         <img
-          class="absolute w-full h-128 rounded-lg object-cover object-top border-b shadow-md img"
+          class="absolute w-full h-128 rounded-lg object-cover object-top border-b shadow-2xl img z-10"
           src="../../assets/images/ken-face.jpg"
         />
       </div>
 
       <div class="px-4 relative">
         <div class="bg-white rounded-lg shadow-2xl border">
-          <div class="p-5"></div>
-
-          <div class="pb-3 px-4">
-            <div>
+          <div class="p-4">
+            <div class="mt-20">
               <div class="text-gray-800 text-lg mb-5">
                 <span class="font-medium uppercase">About Ken</span>
                 <CustomBorder />
               </div>
 
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat earum totam at esse, suscipit explicabo sequi quod!
-                Voluptate mollitia expedita dolores pariatur facere distinctio,
-                commodi neque tenetur iste corporis error?
+                A full stack developer with a passion for thoughtful UI design.
               </p>
 
               <p class="mt-2">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat earum totam at esse, suscipit explicabo sequi quod!
-                Voluptate mollitia expedita dolores pariatur facere distinctio,
-                commodi neque tenetur iste corporis error?
+                Apart from participating in many tech programs where he has been
+                trained to have both technical and soft skills, he has also
+                acquired high level programming knowledge from taking online
+                courses on Udemy, Pluralsight and Udacity, bagging a Node.js
+                certificate, Expert in JavaScript IQ test and JavaScript
+                Certificate respectively.
+              </p>
+
+              <p class="mt-2">
+                When you engage his services for your project, you can rest
+                assured that he will deliver high-quality content and keep to
+                deadlines. He has worked on many clients' projects, and 95% of
+                his clients have left an excellent remarks for him, which shows
+                just how important he showed that they were to him. He will be
+                bringing his A-game to the table always.
               </p>
             </div>
           </div>
