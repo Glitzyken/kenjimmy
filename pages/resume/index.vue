f15d7515c92073262e4220300a879cda4bc4f09d
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-02-26 23:05:30 +0100

    🧑 Update resume

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -23,7 +23,7 @@
       </div>
       <div class="flex justify-between items-center flex-wrap">
         <a
-          href="https://drive.google.com/file/d/1Rqx1MuXN8oIINTlIOH6x1LCdBK2TFSqZ/view?usp=sharing"
+          href="https://drive.google.com/file/d/1eIZU6MWEFL-hVhKzr2Og4A6Cg5OIb9NL/view?usp=sharing"
           target="_blank"
         >
           <button
