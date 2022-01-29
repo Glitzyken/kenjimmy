85195b6a4735dae96cd61d92b095e0e24c116999
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-01-29 13:29:36 +0100

    Update AboutMe.vue

--- components/ui/AboutMe.vue
+++ components/ui/AboutMe.vue
@@ -8,7 +8,7 @@
           <div class="p-4 grid md:gap-5 grid-cols-1 md:grid-cols-2">
             <img
               class="w-128 rounded-lg"
-              src="https://res.cloudinary.com/dwa1jtluu/image/upload/v1643458789/kenjimmy.me/portfolioMe_wks5gq.jpg"
+              src="https://res.cloudinary.com/dwa1jtluu/image/upload/v1643459345/kenjimmy.me/portfolioMe_ho8140.jpg"
             />
             <div class="pt-10">
               <div>
