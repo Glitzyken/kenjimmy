8b89240dd09e1f2c6d24168cca6d593a3b35ca4d
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-04-27 09:38:56 +0100

    Update index.vue

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -49,7 +49,7 @@
       </div>
       <div class="flex justify-between items-center flex-wrap">
         <a
-          href="https://drive.google.com/file/d/1Ge740vg7xXVb4ZxS7f-0BfLvMQ65ZcCt/view?usp=sharing"
+          href="https://drive.google.com/file/d/1cAntuXsTZIyU40taBfgVt67s-3pB6m6x/view?usp=sharing"
           target="_blank"
         >
           <button
