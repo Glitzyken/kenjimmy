fc96fb0eab8d59c18ff5749f8e7def0b93d07187
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-09-15 15:56:03 +0530

    📷 Change video demo to image

--- components/ui/PortfolioWebApps.vue
+++ components/ui/PortfolioWebApps.vue
@@ -26,10 +26,11 @@
             </div>
           </transition>
 
-          <video class="rounded-lg" autoplay muted loop>
+          <!-- <video class="rounded-lg" autoplay muted loop>
             <source :src="spasora.videoLink" type="video/mp4" />
             Your browser is not supported!
-          </video>
+          </video> -->
+          <img :src="spasora.imgUrl" alt="spasora" class="rounded-lg" />
         </div>
       </a>
 
@@ -53,10 +54,11 @@
             </div>
           </transition>
 
-          <video class="rounded-lg" autoplay muted loop>
+          <!-- <video class="rounded-lg" autoplay muted loop>
             <source :src="bossbus.videoLink" type="video/mp4" />
             Your browser is not supported!
-          </video>
+          </video> -->
+          <img :src="bossbus.imgUrl" alt="bossbus" class="rounded-lg" />
         </div>
       </a>
 
@@ -80,10 +82,11 @@
           </div>
         </transition>
 
-        <video class="rounded-lg" autoplay muted loop>
+        <!-- <video class="rounded-lg" autoplay muted loop>
           <source :src="kenJimmy.videoLink" type="video/mp4" />
           Your browser is not supported!
-        </video>
+        </video> -->
+        <img :src="kenJimmy.imgUrl" alt="kenJimmy" class="rounded-lg" />
       </div>
 
       <a :href="mazino.link" target="_blank">
@@ -106,10 +109,35 @@
             </div>
           </transition>
 
-          <video class="rounded-lg" autoplay muted loop>
+          <!-- <video class="rounded-lg" autoplay muted loop>
             <source :src="mazino.videoLink" type="video/mp4" />
             Your browser is not supported!
-          </video>
+          </video> -->
+          <img :src="mazino.imgUrl" alt="mazino" class="rounded-lg" />
+        </div>
+      </a>
+
+      <a :href="flexicreate.link" target="_blank">
+        <div
+          class="relative pf-card rounded-lg"
+          @mouseenter="showDetails(6)"
+          @mouseleave="hideDetails"
+        >
+          <transition name="slide">
+            <div
+              v-if="showDetials6"
+              class="py-4 px-8 w-full bg-kjColorSecondary bg-opacity-75 absolute bottom-0 z-50 rounded-br-lg rounded-bl-lg"
+            >
+              <h2 class="font-bold text-lg text-kjColorLight capitalize">
+                {{ flexicreate.name }}
+              </h2>
+              <p class="text w-2/3 text-kjColorLight mt-2">
+                {{ flexicreate.techUsed }}
+              </p>
+            </div>
+          </transition>
+
+          <img :src="flexicreate.imgUrl" alt="flexicreate" class="rounded-lg" />
         </div>
       </a>
     </div>
@@ -117,7 +145,13 @@
 </template>
 
 <script>
-import { spasora, bossbus, kenJimmy, mazino } from '~/docs/portfolios.js';
+import {
+  spasora,
+  bossbus,
+  kenJimmy,
+  mazino,
+  flexicreate
+} from '~/docs/portfolios.js';
 
 export default {
   data() {
@@ -127,10 +161,12 @@ export default {
       showDetials3: false,
       showDetials4: false,
       showDetials5: false,
+      showDetials6: false,
       spasora: spasora,
       bossbus: bossbus,
       kenJimmy: kenJimmy,
       mazino: mazino,
+      flexicreate: flexicreate,
       message: 'Ken appreciates you 😎. Thanks for visiting! :)',
       showAlert: false
     };
@@ -156,6 +192,10 @@ export default {
       if (num == 5) {
         this.showDetials5 = true;
       }
+
+      if (num == 6) {
+        this.showDetials6 = true;
+      }
     },
     hideDetails() {
       this.showDetials1 = false;
@@ -163,6 +203,7 @@ export default {
       this.showDetials3 = false;
       this.showDetials4 = false;
       this.showDetials5 = false;
+      this.showDetials6 = false;
     },
     showHideAlert() {
       this.showAlert = true;
--- docs/portfolios.js
+++ docs/portfolios.js
@@ -4,12 +4,16 @@ export const mazino = {
   videoLink:
     'https://res.cloudinary.com/dwa1jtluu/video/upload/v1640766642/kenjimmy.me/Mazino_Makeovers____Book_a_makeover_appointment_with_the_best_beauty_saloon_in_Lagos_-_Google_Chrome_2021-10-22_22-26-47compress_nqdqgc.mp4',
   link: 'https://mazinomakeovers.netlify.app',
-  techUsed: 'Nuxt Tailwind Node.js Moment.js'
+  techUsed: 'Nuxt Tailwind Node.js Moment.js',
+  imgUrl:
+    'https://res.cloudinary.com/dwa1jtluu/image/upload/v1663252544/kenjimmy.me/444_a17rec.jpg'
 };
 
 export const kenJimmy = {
   videoLink:
-    'https://res.cloudinary.com/dwa1jtluu/video/upload/v1640766644/kenjimmy.me/DARKMODE.mp4_bj94zf.mp4'
+    'https://res.cloudinary.com/dwa1jtluu/video/upload/v1640766644/kenjimmy.me/DARKMODE.mp4_bj94zf.mp4',
+  imgUrl:
+    'https://res.cloudinary.com/dwa1jtluu/image/upload/v1663252535/kenjimmy.me/112_xoqgia.jpg'
 };
 
 export const spasora = {
@@ -17,7 +21,9 @@ export const spasora = {
   videoLink:
     'https://res.cloudinary.com/dwa1jtluu/video/upload/v1640766649/kenjimmy.me/Spasora___Marketplace_to_discover_and_book_physical_spaces_-_Google_Chrome_2021-10-22_19-48-03.compressed.mp4_gho0ql.mp4',
   link: 'https://spasora.com/',
-  techUsed: 'Nuxt.js TailwindCSS Chakra-UI Axios Node.js Express MongoDB'
+  techUsed: 'Nuxt.js TailwindCSS Chakra-UI Element-UI Node.js Express MongoDB',
+  imgUrl:
+    'https://res.cloudinary.com/dwa1jtluu/image/upload/v1663252550/kenjimmy.me/333_kq5r5b.jpg'
 };
 
 export const bossbus = {
@@ -25,7 +31,17 @@ export const bossbus = {
   videoLink:
     'https://res.cloudinary.com/dwa1jtluu/video/upload/v1642430641/kenjimmy.me/bossbus___ride_charter_-_google_chrome_2022-01-17_15-35-19.mp4_360p_ixizhq.mp4',
   link: 'https://bossbusworld.com/',
-  techUsed: '**classified'
+  techUsed: '**classified',
+  imgUrl:
+    'https://res.cloudinary.com/dwa1jtluu/image/upload/v1663252537/kenjimmy.me/222_c7tsjz.jpg'
+};
+
+export const flexicreate = {
+  name: 'Flexicreate',
+  link: 'https://flexicreate.com/',
+  techUsed: 'Vanila HTML, CSS, and JavaScript',
+  imgUrl:
+    'https://res.cloudinary.com/dwa1jtluu/image/upload/v1663252564/kenjimmy.me/555_ahwodb.jpg'
 };
 
 // NATIVE APPS
